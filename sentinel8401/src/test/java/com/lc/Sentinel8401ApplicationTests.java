f64a0b91ea528557ff36c19d9c53d8e083e1c758
lucong <1193429990@qq.com>
2021-04-18 20:36:39 +0800

    sentinel

---
+++ sentinel8401/src/test/java/com/lc/Sentinel8401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sentinel8401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
